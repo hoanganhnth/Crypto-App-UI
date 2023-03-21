97a4414b53bbf8d8330de38a7f94bc44768eca55
hoanganhnth <hoanganhnth2k3@gmail.com>
2023-03-21 09:42:06 +0700

    main and login

--- app/src/main/java/com/example/cryptoappui/MainActivity.java
+++ app/src/main/java/com/example/cryptoappui/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.cryptoappui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView next;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        next = findViewById(R.id.next);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,LoginActivity.class));
+            }
+        });
+
     }
 }
